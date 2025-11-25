f8bb19b34320250dff00c2d75b5dbcb24e42c91e
ggeorgako <giannhsgew@gmail.com>
2025-11-25 12:03:31 +0200

    log url path to identify the error better

--- backend/internal/middleware/recover.go
+++ backend/internal/middleware/recover.go
@@ -12,7 +12,7 @@ func RecoveryMiddleware(next http.HandlerFunc, db *sql.DB, l *slog.Logger) http.
 	return func(w http.ResponseWriter, r *http.Request) {
 		defer func() {
 			if err := recover(); err != nil {
-				l.Error("server panic", "method", r.Method, "stacktrace", err)
+				l.Error("server panic", "method", r.Method, "paht", r.URL.Path, "stacktrace", err)
 				utils.Internal(w, errors.New("internal server error"))
 			}
 		}()
