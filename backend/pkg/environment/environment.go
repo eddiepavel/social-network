eadbe5ec3d89c2a5f96d3faec61bd94b6fed2763
Graffalo92 <giannhsgew@gmail.com>
2026-01-11 14:21:47 +0200

    stronger environment set

--- backend/pkg/environment/environment.go
+++ backend/pkg/environment/environment.go
@@ -3,11 +3,12 @@ package environment
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"os"
 	"strings"
 )
 
-func SetEnv(filePath string) error {
+func SetEnv(filePath string, compare string) error {
 	file, err := os.Open(filePath)
 
 	if err != nil {
@@ -16,15 +17,49 @@ func SetEnv(filePath string) error {
 
 	scanner := bufio.NewScanner(file)
 
+	var envKeys []string
+	compareKeys := []string{"PORT", "DATABASE_NAME", "PRODUCTION", "APP_URL", "SECRET_KEY"}
+
+	lineNum := 0
+
 	for scanner.Scan() {
+		lineNum++
 		line := strings.Split(scanner.Text(), "=")
 
-		if len(line) != 2 {
-			return errors.New("env file corrupted")
+		switch {
+		case len(line) != 2:
+			return fmt.Errorf("env file corrupted")
+		case len(line[0]) <= 0:
+			return fmt.Errorf("missing key in line %d", lineNum)
+		case len(line[1]) <= 0:
+			return fmt.Errorf("missing value in %s", line[0])
 		}
+
+		envKeys = append(envKeys, line[0])
 
 		os.Setenv(line[0], line[1])
 	}
 
+	missing := findMissing(compareKeys, envKeys)
+	if len(missing) > 0 {
+		return fmt.Errorf("missing required environment variables: %v", missing)
+	}
+
 	return nil
+}
+
+func findMissing(required []string, actual []string) []string {
+	actualMap := make(map[string]bool)
+	for _, key := range actual {
+		actualMap[key] = true
+	}
+
+	var missing []string
+	for _, key := range required {
+		if !actualMap[key] {
+			missing = append(missing, key)
+		}
+	}
+
+	return missing
 }
